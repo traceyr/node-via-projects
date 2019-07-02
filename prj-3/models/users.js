0084ba3dbc8eb71bb2a228dd5067598e1f112694
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-07-02 15:42:53 -0700

    completed project

--- prj-3/models/users.js
+++ prj-3/models/users.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+
 
 mongoose.connect('mongodb://localhost/nodeauth', { useNewUrlParser: true }).then(
   () => { console.log('connected successfully'); },
@@ -8,6 +10,7 @@ mongoose.connect('mongodb://localhost/nodeauth', { useNewUrlParser: true }).then
 );
 
 let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 //User Schema
 let UserSchema = mongoose.Schema({
@@ -31,6 +34,26 @@ let UserSchema = mongoose.Schema({
 
 let User =  module.exports = mongoose.model('User', UserSchema);
 
+module.exports.getUserById = function(id, cb) {
+  User.findById(id, cb);
+};
+
+module.exports.getUserByUsername = function(username, cb) {
+  let query = { username: username };
+  User.findOne(query, cb);
+};
+
+module.exports.comparePassword = function(candidatePW, hash, cb) {
+  bcryptjs.compare(candidatePW, hash, function(err, isMatch) {
+    cb(null, isMatch);
+  });
+};
+
 module.exports.createUser = function(newUser, cb) {
-  newUser.save(cb);
+  bcryptjs.genSalt(10, function(err, salt) {
+    bcryptjs.hash(newUser.password, salt, function(err, hash){
+      newUser.password = hash;
+      newUser.save(cb);
+    });
+  });
 };
--- prj-3/routes/users.js
+++ prj-3/routes/users.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-let uploads = multer({ dest: './uploads' })
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+let uploads = multer({ dest: './uploads' });
 
 let User = require('../models/users');
 
@@ -20,6 +22,36 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: 'Invalid Username or Password' }),
+  function(req, res) {
+    console.log('here');
+    req.flash('success', 'You are now logged in');
+    res.redirect('/');
+  }
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.getUserByUsername(username, function(err, user) {
+    if(err) throw err;
+    if(!user) return done(null, false, { message: 'Unknown User'});
+    User.comparePassword(password, user.password, function(err, isMatch){
+      if(err) return done(err);
+      isMatch ? done(null, user) : done(null, false, { message: 'Invalid Password' });
+    });
+  });
+}));
+
 router.post('/register', uploads.single('profileimg'), function(req, res, next) {
   let name = req.body.name;
   let email = req.body.email;
@@ -33,7 +65,7 @@ router.post('/register', uploads.single('profileimg'), function(req, res, next) 
     profileimg = req.file.filename;
   } else {
     console.log('No File Uploading...');
-    profileimg = 'noimage.jpg'
+    profileimg = 'noimage.jpg';
   }
   //Form validator
   req.checkBody('name', 'Name field is required').notEmpty();
@@ -46,10 +78,12 @@ router.post('/register', uploads.single('profileimg'), function(req, res, next) 
   let errors = req.validationErrors();
 
   if(errors) {
+    console.log('err');
     res.render('register', {
       errors: errors
     });
   } else {
+    console.log('creating user');
     let newUser = new User({
       name: name,
       email: email,
@@ -68,6 +102,12 @@ router.post('/register', uploads.single('profileimg'), function(req, res, next) 
     res.location('/');
     res.redirect('/');
   }
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  req.flash('success', 'You have successfully logged out');
+  res.redirect('/users/login');
 });
 
 module.exports = router;
