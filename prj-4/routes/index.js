4c9156cd6eddc8c601b2ded6eaf638f826f52fc8
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-07-24 17:44:53 -0700

    retreives posts from the db

--- prj-4/routes/index.js
+++ prj-4/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+let express = require('express');
+let router = express.Router();
+let mongo = require('mongodb');
+let db = require('monk')('localhost/nodeblog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let db = req.db;
+  let posts = db.get('posts');
+  posts.find({}, {}, function(err, posts) {
+    res.render('index', { posts: posts });
+  });
+
 });
 
 module.exports = router;
